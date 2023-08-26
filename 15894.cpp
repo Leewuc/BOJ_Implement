a06e287d289d10c9aef381c6b9356790a4ec9549
Leeuc <92780253+Leewuc@users.noreply.github.com>
2023-08-26 18:07:17 +0900

    Create 15894.cpp

---
+++ 15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void fast(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main(){
+    fast();
+    long long n;
+    cin >> n;
+    cout << n*4;
+    return 0;
+}
