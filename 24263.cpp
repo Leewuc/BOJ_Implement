62c03a9313338081133269ba514275d7fd6dce44
Leeuc <92780253+Leewuc@users.noreply.github.com>
2023-10-05 03:04:17 +0000

    Create 24263.cpp

---
+++ 24263.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void fast(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+int main(){
+    fast();
+    int n;
+    cin >> n;
+    cout << n << '\n' << 1;
+    return 0;
+}
