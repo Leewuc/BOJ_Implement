cf31a37a90a29089cf6deacd729711858dac2acd
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-01-07 18:17:42 +0900

    Create 10886.py

---
+++ 10886.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = 0
+b = 0
+for _ in range(n):
+    if(int(input())) == 1:
+        a += 1
+    else:
+        b += 1
+if(a>b):
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
