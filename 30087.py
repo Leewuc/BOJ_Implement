18bed69cd422b334899cd02cde7eb420bfdef97c
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-05-08 13:14:59 +0900

    Create 30087.py

---
+++ 30087.py
@@ -0,0 +1,17 @@
+n = int(input())
+for i in range(n):
+    n1 = input()
+    if n1 == 'Algorithm':
+        print("204")
+    elif n1 == 'DataAnalysis':
+        print("207")
+    elif n1 == 'ArtificialIntelligence':
+        print("302")
+    elif n1 == 'CyberSecurity':
+        print("B101")
+    elif n1 == 'Network':
+        print("303")
+    elif n1 == 'Startup':
+        print("501")
+    elif n1 == 'TestStrategy':
+        print("105")
